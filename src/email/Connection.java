5c15a7d825764642bd7e4ffd79f17b3b2f3c73c1
YasserSyr <blackheart-y@hotmail.com>
2015-04-13 17:14:32 +0300

    test2

--- src/email/Connection.java
+++ src/email/Connection.java
@@ -11,18 +11,8 @@ public class Connection {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Connection window = new Connection();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
+	
 
 	/**
 	 * Create the application.
